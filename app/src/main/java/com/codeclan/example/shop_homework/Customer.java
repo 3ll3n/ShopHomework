1c0349a9bfdc0cf9b23dd5a9b7f31cbce017a394
3ll3n <ellengrafton01@gmail.com>
2017-02-26 15:36:39 +0000

    Added methods and tests to customer for getName, getMoney and buy

--- app/src/main/java/com/codeclan/example/shop_homework/Customer.java
+++ app/src/main/java/com/codeclan/example/shop_homework/Customer.java
@@ -5,4 +5,24 @@ package com.codeclan.example.shop_homework;
  */
 
 public class Customer {
+
+    private String name;
+    private int money;
+
+    public Customer(String name, int money) {
+        this.name = name;
+        this.money = money;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void buy(int purchase) {
+        this.money -= purchase;
+    }
 }
--- app/src/test/java/com/codeclan/example/shop_homework/CustomerTest.java
+++ app/src/test/java/com/codeclan/example/shop_homework/CustomerTest.java
@@ -1,8 +1,39 @@
 package com.codeclan.example.shop_homework;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by user on 26/02/2017.
  */
 
 public class CustomerTest {
+
+    Customer customer1;
+    Customer customer2;
+
+    @Before
+    public void before() {
+        customer1 = new Customer("Anna", 500);
+        customer2 = new Customer("Elsa", 450);
+    }
+
+
+    @Test
+    public void testGetName() {
+        assertEquals("Anna", customer1.getName());
+    }
+
+    @Test
+    public void testGetTotalMoney() {
+        assertEquals(450, customer2.getMoney());
+    }
+
+    @Test
+    public void testBuy() {
+        customer1.buy(200);
+        assertEquals(300, customer1.getMoney());
+    }
 }
