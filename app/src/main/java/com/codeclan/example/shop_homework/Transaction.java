39473661b91308e97e63760a16bbe55aa4d58302
3ll3n <ellengrafton01@gmail.com>
2017-02-26 16:39:14 +0000

    Updated Transactions filename to Transaction

---
+++ app/src/main/java/com/codeclan/example/shop_homework/Transaction.java
@@ -0,0 +1,19 @@
+package com.codeclan.example.shop_homework;
+
+import java.util.ArrayList;
+
+/**
+ * Created by user on 26/02/2017.
+ */
+
+public class Transaction {
+
+    private ArrayList<Integer> sales;
+    private ArrayList<Integer> refunds;
+
+    public Transaction() {
+
+    }
+
+
+}
